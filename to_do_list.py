474ba6cc61bbfebf5bf6b923e3c4a64c107c54e1
visheshgubrani <33202551+visheshgubrani@users.noreply.github.com>
2023-05-25 23:10:02 +0530

    started the project

--- to_do_list.py
+++ to_do_list.py
@@ -1,17 +1,33 @@
 tasks = []
 
-def add_tasks():
-    task = input("Add a task: ")
+def display_menu():
+    print("To-Do List")
+    print("==========")
+    print("1. Add a Task")
+    print("2. Delete a task")
+    print("3. Edit a task")
+    print("4. Mark a task as done")
+    print("5. View all tasks")
+    print("6. Exit")
+
+def add_task():
+    name = input("Enter a task here")
+    priority = input("Enter priority high, medium or low: ")
+
+    task = {
+        "name": name,
+        "priority": priority,
+        "status" : "pending"
+    }
+
     tasks.append(task)
-    print("Task addes Successfully")
 
-def display_task():
+    print(f"The {name} added successfully")
+
+def delete_task():
     if len(tasks) == 0:
-        print("No tasks found")
+        print("There are no tasks to delete")
     else:
-        print("Current Tasks")
-        for index, task in enumerate(tasks):
-            status = "[X]" if task ["status"] == "Done" else "[ ]"
-            print(f"{index}. {status} {task['name']}")
+        for i in range(len(tasks)):
+            print(f"{i+1}. {tasks[i]['name']} - {tasks[i]['priority']} - {tasks[i]['status']}")
 
-        choice = input("Enter the index of the toggle ")
